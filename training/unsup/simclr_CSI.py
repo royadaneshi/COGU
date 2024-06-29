bdbab155fa452ed63e8ca62f0128383e2315880b
your_username <royadaneshi2001@gmail.com>
2024-06-29 13:28:44 +0330

    batch resolve problem

--- training/unsup/simclr_CSI.py
+++ training/unsup/simclr_CSI.py
@@ -80,7 +80,7 @@ def train(P, epoch, model, criterion, optimizer, scheduler, loader, train_exposu
         sim_matrix = get_similarity_matrix(simclr, multi_gpu=P.multi_gpu)
         loss_sim = NT_xent(sim_matrix, temperature=0.5) * P.sim_lambda
 
-        ##################
+        ################
         shift_labels = shift_labels[:outputs_aux['shift'].size(0)]
         ##################
         loss_shift = criterion(outputs_aux['shift'], shift_labels)
