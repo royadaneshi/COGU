83cfe749d7f71348b1c576057a63e11722031578
your_username <royadaneshi2001@gmail.com>
2024-07-02 11:17:55 +0330

    batch debug problem3

--- training/unsup/simclr_CSI.py
+++ training/unsup/simclr_CSI.py
@@ -93,8 +93,8 @@ def train(P, epoch, model, criterion, optimizer, scheduler, loader, train_exposu
 
         # Ensure they have the same batch size
         min_size = min(outputs_shift_flat.size(0), shift_labels_flat.size(0))
-        if min_size >= 28:
-            min_size = 28
+        if min_size >= 4:
+            min_size = 4
 
         outputs_shift_flat = outputs_shift_flat[:min_size]
         shift_labels_flat = shift_labels_flat[:min_size]
